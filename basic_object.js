bacb09c651fbe8f30a1b27b25456f74db2c26744
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-30 09:05:39 +0100

    Update basic_object.js

--- basic_object.js
+++ basic_object.js
@@ -2,4 +2,4 @@ let duck = {
   name: "Aflac",
   numLegs: 2
 };
-This duck object has two property/value pairs: a name of Aflac and a numLegs of
+//This duck object has two property/value pairs: a name of Aflac and a numLegs of
