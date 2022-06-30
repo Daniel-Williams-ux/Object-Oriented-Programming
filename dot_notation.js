9c6c804971f013909cec9957443b5c49bf3c5eaf
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-30 09:29:44 +0100

    Update dot_notation.js

--- dot_notation.js
+++ dot_notation.js
@@ -3,3 +3,16 @@ let duck = {
   numLegs: 2
 };
 console.log(duck.name);//Aflac
+
+
+let dog = {
+  name: "spots",
+  numLegs: 4
+}
+
+let result1 = dog.name;
+console.log(result1);
+
+let result2 = dog.numLegs;
+console.log(result2);
+
